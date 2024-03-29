07d04e3cdb026997a819704a39f7ad8a343042e0
sushil-arya <sushilarya888@gmail.com>
2024-03-29 07:14:29 +0530

    url_Correct1

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />}></Route>
-          <Route path="/post" element={<Post />}></Route>
-          <Route path="/about" element={<About />}></Route>
-          <Route path="/contact" element={<Contact />}></Route>
+          <Route path="post" element={<Post />}></Route>
+          <Route path="about" element={<About />}></Route>
+          <Route path="contact" element={<Contact />}></Route>
         </Route>
       </Routes>
     </BrowserRouter>
